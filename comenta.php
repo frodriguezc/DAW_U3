849623a19c6370ce717a15fc296b9e5fd39300c4
Felipe Rodriguez Carcher <frodriguezcarcher@gmail.com>
2021-07-31 17:54:24 -0400

    update 9

--- comenta.php
+++ comenta.php
@@ -1,22 +1,29 @@
 <?php
-	//Insertar un registro de alumno en la tabla.
-	include("cx.php");
-	$link = conectar();
-	//Se recuperan los parámetros desde la petición.
-	$nombre = $_GET["nombre"];
-	$email = $_GET["correo"];
-	$comentario = $_GET["comentarios"];
-	//Ejecutar la sentencia de inserción en la tabla.
-	$insert = "insert into comentarios(nombre, email, comentario) values('{$nombre}','{$email}','{$comentario})'";
-	//Si la inserción se lleva a cabo resultado es mayor que 0.
-	if (!mysqli_query($link, $insert)){
-		//Si la inserción se lleva a cabo correctamente se genera un mensaje como resultado.
-        $resultado = "Inserción ERROR";
-		$error = mysqli_error($link);
-	} else {
-		$resultado = "Inserción OK";
-        $error ="Inserción OK";
-    }
-	mysqli_close($link);
-	header("Location:contacto.php?dato={$resultado}&error={$error}");
+//Insertar un registro de alumno en la tabla.
+include("cx.php");
+$link = conectar();
+//Se recuperan los parámetros desde la petición.
+$nombre = $_GET["nombre"];
+$email = $_GET["correo"];
+$comentario = $_GET["comentarios"];
+//Ejecutar la sentencia de inserción en la tabla.
+$insert = "insert into comentarios(nombre, email, comentario) values('{$nombre}','{$email}','{$comentario}')";
+//Si la inserción se lleva a cabo resultado es mayor que 0.
+if (!mysqli_query($link, $insert)) {
+    //Si la inserción se lleva a cabo correctamente se genera un mensaje como resultado.
+    $error = mysqli_error($link);
+    mysqli_close($link);
+
+    echo '<script type="text/javascript">';
+    echo 'alert("Problema $error")';
+    echo '</script>';
+} else {
+    $resultado = "Comentario Recibido, muchas gracias!";
+    echo "<script>
+alert('{$resultado}');
+window.location.href='index.php';
+</script>";
+}
+mysqli_close($link);
+die();
 ?>
